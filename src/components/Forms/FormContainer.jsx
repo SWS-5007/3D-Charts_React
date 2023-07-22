59fc25e405491d6595a2a01b1a23c15f30b6603f
Vijay <vijayvermakvp@gmail.com>
2023-07-22 16:39:18 +0530

    Adding Query String

--- src/components/Forms/FormContainer.jsx
+++ src/components/Forms/FormContainer.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Line from "./Line";
 import Pie from "./Pie";
+import queryString from "query-string";
 
 const FormContainer = () => {
+  const search = queryString.parse(window.location.search);
+
   return (
     <>
-      <Pie />
-      <Line />
+      {search.type === "line" && <Line />}
+      {search.type === "pie" && <Pie />}
     </>
   );
 };
